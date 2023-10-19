57e50bcdc54c0adc26605f4e369f8566215a0bdb
deepak2442 <deepakvpoo1999@gmail.com>
2023-10-19 23:35:38 +0530

    laying the foundation

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
-const heading= React.createElement("h1",{id:"heading"},"Namste React")
+// const heading= React.createElement("h1",{id:"heading"},"Namste React")
+
+// jsx (transpiled before it reaches the js)-PARCEL-Babel
+
+// jsx => React.createElement. JS object => HTMLelement(render)
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
 
  const root=ReactDOM.createRoot(document.getElementById("root"));
 
-        root.render(heading)
+//  React component
+const HeadingComponent = () =>(
+    <div className="container">
+        <h1> namste react functional component</h1>
+    </div>
+)
+
+  root.render(<HeadingComponent/>)
