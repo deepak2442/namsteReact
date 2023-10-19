1a564c952a2659a6ad2ea6d67d60d1342b71142a
deepak2442 <deepakvpoo1999@gmail.com>
2023-10-19 09:50:28 +0530

    episode-1

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-// <div id="parent">
-// 
 import React from "react";
-import { ReactDOM } from "react";
+import  ReactDOM  from "react-dom";
 
+const heading= React.createElement("h1",{id:"heading"},"Namste React")
 
-
-
-
-const heading = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"im h1 from my aprent"),
-        React.createElement("h2",{},"im h2 from my aprent")
-    ]), React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"im h1 from my aprent"),
-        React.createElement("h2",{},"im h2 from my aprent")
-    ])
-])
-
-        const root=ReactDOM.createRoot(document.getElementById("root"));
+ const root=ReactDOM.createRoot(document.getElementById("root"));
 
         root.render(heading)
